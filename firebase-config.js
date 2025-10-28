e529f05c6f838b124ec2e86ff768b42fc0158eea
kerepek-kedai-saya <ozzyosbakar@gmail.com>
2025-10-28 21:26:38 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCERTkaGT0McKq_ryQv1Zak1lIFGkzR7EM",
+  authDomain: "thekrepeqho.firebaseapp.com",
+  databaseURL: "https://thekrepeqho-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "thekrepeqho",
+  storageBucket: "thekrepeqho.firebasestorage.app",
+  messagingSenderId: "300627856463",
+  appId: "1:300627856463:web:cef2c8daf14158fd9b2e3a",
+  measurementId: "G-M9QPK32GPH"
+};
